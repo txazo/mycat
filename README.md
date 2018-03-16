23382800c300a8d4317331ddfde02c1ee17a3d24
txazo <784990655@qq.com>
2018-03-16 19:38:57 +0800

    update conf

---
+++ README.md
@@ -0,0 +1,7 @@
+## Mycat
+
+Mycat
+
+## 说明
+
+* 版本: 1.6.5-RELEASE
--- src/test/java/io/mycat/test/MycatStartupTest.java
+++ src/test/java/io/mycat/test/MycatStartupTest.java
@@ -1,4 +1,14 @@
 package io.mycat.test;
 
+import io.mycat.MycatStartup;
+
 public class MycatStartupTest {
+
+    /**
+     * VM Args: -DMYCAT_HOME=//Users/txazo/TxazoProject/mycat -server -Xms1G -Xmx1G -XX:MaxPermSize=64M  -XX:+AggressiveOpts -XX:MaxDirectMemorySize=1G
+     */
+    public static void main(String[] args) {
+        MycatStartup.main(new String[]{"start"});
+    }
+
 }
